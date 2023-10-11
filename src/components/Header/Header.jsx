74de5a65659cc6cb35534aa14f3e57ea36579487
Maria Zak <mzakart@gmail.com>
2023-10-11 14:53:43 -0400

    Styling header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Logo from "../../assets/icons/drama.png"
 import "./Header.scss";
 
 function Header() {
   return (
-    <header>
-      <h1>Tim's Site</h1>
+    <header className='header'>
+      <img src={Logo} alt="logo" className='header-logo'></img>
+      <div className='header-title'>
+        <h1 >Canadian Broadway Theatre</h1>  
+        <h2>Monologue Competition</h2>
+        </div>
+      
+      <div className='header-nav'></div>
     </header>
   );
 }
