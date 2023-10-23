548dde7f80f71e684eae4f13a610978699674c06
Maria Zak <mzakart@gmail.com>
2023-10-22 20:26:04 -0400

    Stripe payments - contestant, votes

--- src/components/StripeWrapper/StripeWrapper.jsx
+++ src/components/StripeWrapper/StripeWrapper.jsx
@@ -1,8 +1,11 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromises = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
-const StripeWrapper = ( { children}) =>{
+const stripePromises = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY).catch(error => {
+    console.error("Stripe initialization error:", error);
+  });
+  
+  const StripeWrapper = ( { children}) =>{
     return <Elements stripe={stripePromises} > { children} </Elements>
 }
 
