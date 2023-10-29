618667c689d46b54078f5ac95e6e3acfcd4fd1c4
Maria Zak <mzakart@gmail.com>
2023-10-29 19:15:40 -0400

    payment button styling

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
 import { auth } from '../../firebase';
 import { currentUser } from 'firebase/auth';
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useStripe, useElements } from "@stripe/react-stripe-js";
 import { CardNumberElement, CardExpiryElement, CardCvcElement } from "@stripe/react-stripe-js";
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
 import "../../styles/forms.scss";
 
+// photo upload
+    const BUCKET_NAME = process.env.REACT_APP_AWS_BUCKET_NAME;
+    const REGION = process.env.REACT_APP_AWS_REGION;
+    const s3Client = new S3Client({ region: REGION}); 
+    const MAX_SIZE = 5 * 1024 * 1024;
+
+
 function UploadForm({backendURL}) {
 
+    const [uploadStatus, setUploadStatus] = useState("idle");
+    //payments
     const stripe = useStripe();
     const elements = useElements();
-
+    
     const [formData, setFormData] = useState({
         photoUrl: '', // Keeping this, will be null for now.
         videoUrl: '',
@@ -38,6 +48,88 @@ function UploadForm({backendURL}) {
         }
     };
 
+    const uploadToS3 = async (file) =>{
+        setUploadStatus("uploading");
+
+        try{
+            const uploadKey = `${Date.now()}-${file.name}`;
+            const command = new PutObjectCommand({
+                Bucket: BUCKET_NAME,
+                Key: uploadKey,
+                Body: file
+            });
+            await s3Client.send(command);
+            const fileURL = `https://${BUCKET_NAME}.s3.${REGION}.amazonaws.com/${uploadKey}`;
+            setUploadStatus("success");
+            return fileURL;
+        }catch (error) {
+            console.error("Error uploading to S3:", error);
+            setUploadStatus("failed");
+
+            return null;
+        }
+    };
+
+    const compressImage = (file, callback) =>{
+        const img =new Image();
+        img.src =URL.createObjectURL(file);
+        img.onload = () =>{
+            const canvas = document.createElement("canvas");
+            const MAX_WIDTH = 400;
+            const MAX_HEIGHT = 400; 
+
+            let width = img.width;
+            let height = img.height;
+
+            
+        if (width > height) {
+            if (width > MAX_WIDTH) {
+                height *= MAX_WIDTH / width;
+                width = MAX_WIDTH;
+            }
+        } else {
+            if (height > MAX_HEIGHT) {
+                width *= MAX_HEIGHT / height;
+                height = MAX_HEIGHT;
+            }
+        }
+
+        canvas.width = width;
+        canvas.height = height; 
+
+        const ctx =canvas.getContext("2d");
+        ctx.drawImage(img, 0,0, width, height);
+
+        //convert canvas into blob
+        canvas.toBlob(callback, "image/jpeg", 0.7); 
+        };
+    };
+
+
+    const handlePhotoChange = async (e) => {
+        const file = e.target.files[0];
+        compressImage(file, async (blob) => {
+            const url = await uploadToS3(blob)
+        });
+
+        if (file.size > MAX_SIZE) {  // Assuming MAX_SIZE is defined
+            alert("File is too large!");
+            return;
+        }
+        if (!["image/jpeg", "image/png"].includes(file.type)) {
+            alert("Invalid file type!");
+            return;
+        }
+
+        const url = await uploadToS3(file);
+        if (url) {
+            setFormData(prevState => ({
+                ...prevState, 
+                photoUrl: url
+            }));
+        }
+    }
+    
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setFormData(prevState => ({
@@ -124,22 +216,21 @@ function UploadForm({backendURL}) {
             }
         }
     }
-
     return (
         <div className="form-container">
             <h2 className="form-container__title">Upload to Enter Contest</h2>
+            
             {!hasPaid ? (
                 <div className="form-container__payment-info">
                     <h3 className="form-container__instruction">Please pay to participate in the contest</h3>
                     <label>
-                Name on Card
-                <input 
-                    className="form-container__input form-container__input--text"
-                    type="text" 
-                    placeholder="Name" 
-                    // Add an onChange handler and value binding if you want to manage this state
-                />
-            </label>
+                        Name on Card
+                        <input 
+                            className="form-container__input form-container__input--text"
+                            type="text"
+                            placeholder="Name"
+                        />
+                    </label>
                     <label>
                         Card number
                         <CardNumberElement options={CARD_STYLES} />
@@ -152,50 +243,71 @@ function UploadForm({backendURL}) {
                         CVC
                         <CardCvcElement options={CARD_STYLES} />
                     </label>
-                    <button className="form-container__pay-button" onClick={handlePayment}>Pay To Enter and Upload</button>
-                </div>
+
+                    <button className="form-container__form__submit-button" onClick={handlePayment}>Pay To Enter and Upload</button>
+
+                    
+                </div> 
             ) : (
+
+
+
+
                 <form className="form-container__form" onSubmit={handleSubmit}>
-
-<div className="input-group">
-
-                    <input
-                        className="form-container__input form-container__input--text"
-                        type="text"
-                        name="videoUrl"
-                        placeholder="Video URL"
-                        value={formData.videoUrl}
-                        onChange={handleInputChange}
-                    />
+                    <div>
+    {uploadStatus === "uploading" && <p>Uploading...</p>}
+    {uploadStatus === "success" && <p>Upload Successful!</p>}
+    {uploadStatus === "failed" && <p>Upload Failed. Please try again.</p>}
+</div>
+                    <div className="input-group">
+                        <label>Upload Photo:</label>
+                        <input 
+                            type="file" 
+                            accept="image/*" 
+                            onChange={handlePhotoChange}
+                        />
                     </div>
 
                     <div className="input-group">
-
-                    <input
-                        className="form-container__input form-container__input--text"
-                        type="text"
-                        name="description"
-                        placeholder="Description"
-                        value={formData.description}
-                        onChange={handleInputChange}
-                    />
+                        <input
+                            className="form-container__input form-container__input--text"
+                            type="text"
+                            name="videoUrl"
+                            placeholder="Video URL"
+                            value={formData.videoUrl}
+                            onChange={handleInputChange}
+                        />
                     </div>
+
                     <div className="input-group">
-
-                    <input
-                        className="form-container__input form-container__input--text"
-                        type="text"
-                        name="name"
-                        placeholder="Name"
-                        value={formData.name}
-                        onChange={handleInputChange}
-                    />
+                        <input
+                            className="form-container__input form-container__input--text"
+                            type="text"
+                            name="description"
+                            placeholder="Description"
+                            value={formData.description}
+                            onChange={handleInputChange}
+                        />
                     </div>
+
+                    <div className="input-group">
+                        <input
+                            className="form-container__input form-container__input--text"
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                            value={formData.name}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+
                     <button className="form-container__submit-button" type="submit">Submit</button>
                 </form>
             )}
         </div>
     );
+    
+    
 }
 
 export default UploadForm;
