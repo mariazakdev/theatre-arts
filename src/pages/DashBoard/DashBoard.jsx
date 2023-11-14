a8eb1773f89b0666efd04d5f8519db6f8e8de038
Maria Zak <mzakart@gmail.com>
2023-11-13 20:37:57 -0500

    Dashbaord works

--- src/pages/DashBoard/DashBoard.jsx
+++ src/pages/DashBoard/DashBoard.jsx
@@ -3,10 +3,39 @@ import { useAuth } from '../../contexts/AuthContext';
 import { Link, useNavigate } from 'react-router-dom';
 
 export default function Dashboard({backendURL}) {
+
   const [error, setError] = useState("");
   const [contestants, setContestants] = useState([]);
   const { currentUser, logout } = useAuth();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUserDashboard = async () => {
+      try {
+        const token = await currentUser.getIdToken();
+        const response = await fetch(`http://localhost:8000/dashboard`, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch dashboard data');
+        }
+
+        const data = await response.json();
+        setContestants(data.contestants);
+      } catch (error) {
+        console.error('Error fetching dashboard data: ', error);
+        setError('Failed to load dashboard data');
+      }
+    };
+
+    if (currentUser) {
+      fetchUserDashboard();
+    }
+  }, [currentUser, backendURL]);
+
 
   async function handleLogout() {
     setError('');
@@ -18,52 +47,29 @@ export default function Dashboard({backendURL}) {
     }
   }
 
-  // Fetch all contestants
-  useEffect(() => {
-    const fetchContestants = async () => {
-      try {
-        // Add a check to ensure currentUser exists before attempting to fetch data
-        if (currentUser) {
-          const response = await fetch(`${backendURL}/uploads/${currentUser.uid}`); // Fetch user-specific data
-          const data = await response.json();
-          setContestants(data);
-        }
-      } catch (error) {
-        console.error('Error retrieving contestants: ', error);
-        setError('Failed to retrieve contestants');
-      }
-    };
-
-    if (currentUser) {
-      fetchContestants();
-    }
-  }, [currentUser, backendURL]);
-
   return (
     <div>
       <p>Dashboard is here</p>
       <strong>Email:</strong> {currentUser && currentUser.email}
-      <Link to="/update-profile">
-        Update Profile
-      </Link>
+      <Link to="/update-profile">Update Profile</Link>
       {contestants.map(contestant => (
         <div key={contestant.id}>
-          <img src={contestant.signedPhotoUrl} alt={contestant.name} />
-          {contestant.url_video && (
-            
-            <iframe
-            title="Contestant Video"
-            width="320"
-            height="240"
-            src={contestant.url_video}
-            allowFullScreen
-          ></iframe>
-
-          )}
+          <h3>{contestant.name}</h3>
           <p>{contestant.description}</p>
+          <img src={contestant.url_photo} alt={contestant.name} style={{ maxWidth: '200px' }} />
+          {contestant.url_video && (
+            <iframe
+              title="Contestant Video"
+              width="320"
+              height="240"
+              src={contestant.url_video}
+              allowFullScreen
+            ></iframe>
+          )}
+          <p>Votes: {contestant.votes}</p>
         </div>
       ))}
-      <button onClick={handleLogout}> Log Out</button>
+      <button onClick={handleLogout}>Log Out</button>
       {error && <p>{error}</p>}
     </div>
   );
