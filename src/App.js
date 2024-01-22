e6d2b8b3b38b9e18868e5c80f38578039ceb1851
Maria Zak <mzakart@gmail.com>
2024-01-21 20:24:53 -0500

    hash

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import PrivateContestantRoute from "./components/PrivateRoute/PrivateContestantRoute";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
