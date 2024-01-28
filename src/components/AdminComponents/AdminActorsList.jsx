9afcd1144cad8912784d70422c6bbb4db5023f35
Maria Zak <mzakart@gmail.com>
2024-01-28 13:40:53 -0500

    admin filters

--- src/components/AdminComponents/AdminActorsList.jsx
+++ src/components/AdminComponents/AdminActorsList.jsx
@@ -3,58 +3,112 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import ContestantStanding from "../ContestantStanding/ContestantStanding";
 import AdminSunKingEdit from "./AdminSunKingEdit";
-import "../../pages/AdminPage/AdminPage.scss";
-
+import "./AdminActorsList.scss";
 
 const URL = process.env.REACT_APP_BACKEND_URL;
 
 function AdminActorsList() {
-    const [videoData, setVideoData] = useState([]);
-    const navigate = useNavigate();
-  
-    useEffect(() => {
-      axios
-        .get(`${URL}/contestants`)
-        .then((response) => {
+  const [videoData, setVideoData] = useState([]);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [currentFilter, setCurrentFilter] = useState("votes"); 
+  useEffect(() => {
+    axios
+      .get(`${URL}/contestants`)
+      .then((response) => {
+        setVideoData(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the video data:", error);
+      });
+  }, [URL]);
+
+  const handleCardClick = (video) => {
+    navigate(`/actors/vote/${video.id}`, { state: { actor: video } });
+  };
+
+  const handleDeleteClick = (actorId) => {
+    axios
+      .delete(`${URL}/contestants/${actorId}`)
+      .then(() => {
+        // Refresh the contestant list after deletion
+        axios.get(`${URL}/contestants`).then((response) => {
           setVideoData(response.data);
-        })
-        .catch((error) => {
-          console.error("There was an error fetching the video data:", error);
         });
-    }, [URL]);
-  
-    const handleCardClick = (video) => {
-      navigate(`/actors/vote/${video.id}`, { state: { actor: video } });
-    };
-  
-    return (
-      <div className="admin-list-container">
-        <h1>Users and their Videos</h1>
-        <ContestantStanding URL={URL} />
-        <div className="admin-cards-container">
-          <AdminSunKingEdit />
-          {videoData.map((video) => (
-            <div
-              key={video.user_id}
-              className="admin-card"
-              onClick={() => handleCardClick(video)}
-            >
-              <div className="admin-card-content">
-                <h2 className="card-title">{video.name}</h2>
-                <img
-                  src={video.url_photo}
-                  alt={video.name}
-                  className="card-image"
-                />
-                <p className="card-description">{video.url_video}</p>
-                <p className="card-description">{video.description}</p>
-                <p className="card-votes">Votes: {video.votes}</p>
-              </div>
-            </div>
-          ))}
-        </div>
+      })
+      .catch((error) => {
+        console.error("Error deleting contestant:", error);
+      });
+  };
+  // Function to handle changing the filter
+  const handleFilterChange = (filter) => {
+    setCurrentFilter(filter);
+    // Adjust the sorting logic based on the selected filter
+    if (filter === "votes") {
+      setVideoData([...videoData].sort((a, b) => b.votes - a.votes));
+    } else if (filter === "alphabetical") {
+      setVideoData([...videoData].sort((a, b) => a.name.localeCompare(b.name)));
+    }
+  };
+
+  return (
+    <div className="admin-actor">
+      <h1>Users and their Videos</h1>
+       {/* Filter buttons */}
+       <div className="filter-buttons">
+        <button onClick={() => handleFilterChange("votes")}>Most Votes</button>
+        <button onClick={() => handleFilterChange("alphabetical")}>Alphabetical Order</button>
       </div>
-    );
+      {/* List of actors */}
+      <div className="admin-actor__card">
+        {videoData.map((video) => (
+          <div
+            key={video.user_id}
+            className="admin-card"
+            onClick={() => handleCardClick(video)}
+          >
+            <div className="admin-actor__card-content">
+              <h2 className="card-title">{video.name}</h2>
+              <img
+                src={video.url_photo}
+                alt={video.name}
+                className="admin-actor__card-image"
+              />
+              <div className="admin-actor__card-description">
+                <strong>Video Link: </strong>
+                <a
+                  href={video.url_video}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {video.url_video}
+                </a>
+                <p>{video.description}</p>
+              </div>
+       
+
+              {video.url_video && (
+                <iframe
+                  title="Contestant Video"
+                  src={video.url_video.replace("watch?v=", "embed/")}
+                  allowFullScreen
+                  className="admin-card-content__video"
+                ></iframe>
+              )}
+
+              <p className="card-votes">Votes: {video.votes}</p>
+            </div>
+            <button
+              className="delete-button"
+              onClick={() => handleDeleteClick(video.id)}
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default AdminActorsList
+export default AdminActorsList;
--- src/components/BannerSunKing/BannerSunKing.jsx
+++ src/components/BannerSunKing/BannerSunKing.jsx
@@ -1,22 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import img from "../../assets/images/fotor-2024010481259.png";
 import "./BannerSunKing.scss";
+const URL = process.env.REACT_APP_BACKEND_URL;
+
 
 function BannerSunKing() {
+
+  const [sunKingData, setSunKingData] = useState({
+    id: 1,
+    title: "",
+    subtitle: "",
+    content: "",
+  });
+
+useEffect(() => {
+  // Fetch Sun King data from the server and update the state
+  axios.get(`${URL}/sun-king`).then((response) => {
+    setSunKingData(response.data[0]);
+  });
+}, []);
+
+
+
   return (
     <section className="baner-snippet">
       <div className="baner-snippet-text">
         <div className="titles">
-          <h2>BANER PRODUCTIONS PRESENTS</h2>
-          <h1>THE SUN KING</h1>
+          <h2>{sunKingData.title}</h2>
+          <h1>{sunKingData.subtitle}</h1>
         </div>
         <div className="baner-content-snippet">
           <p>
-            In the Sun King play, radiant beams of light illuminate the royal
-            court. Vivamus auctor lacus id justo faucibus, in ullamcorper justo
-            fermentum. Nulla facilisi. Etiam ultrices urna in lectus cursus, ac
-            interdum nisi vestibulum...
+          {sunKingData.content}
           </p>
           <Link to="/sun-king">
             <button className="card-button card-button2">Go to Sun King</button>
