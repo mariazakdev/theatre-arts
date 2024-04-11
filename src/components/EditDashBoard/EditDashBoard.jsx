784ac5b4ea417a90aafc09d630b975e02cbd4962
Maria Zak <mzakart@gmail.com>
2024-04-11 18:46:29 -0400

    Video based on rounds

--- src/components/EditDashBoard/EditDashBoard.jsx
+++ src/components/EditDashBoard/EditDashBoard.jsx
@@ -1,32 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './EditDashBoard.scss';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./EditDashBoard.scss";
 
-function EditDashboard({ URL, contestantId, toggleEditing,  updateContestantData, API_KEY }) {
+// NOTE ABOUT VIDEO. For each round manually adjust round === 1/2/3 etc line 16, 136 
 
+function EditDashboard({
+  URL,
+  contestantId,
+  toggleEditing,
+  updateContestantData,
+  API_KEY,
+}) {
   const [updateError, setUpdateError] = useState(null);
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [contestantData, setContestantData] = useState({ round: 1 });
 
   const [formData, setFormData] = useState({
-    description: '',
-    videoUrl: '',
+    description: "",
+    videoUrl: "",
   });
 
   useEffect(() => {
     const fetchContestantData = async () => {
       try {
-        const response = await axios.get(`${URL}/contestants/${contestantId}`,
-        { headers: { Authorization: `${API_KEY}` } }
+        const response = await axios.get(`${URL}/contestants/${contestantId}`, {
+          headers: { Authorization: `${API_KEY}` },
+        });
+        const fetchedContestantData = response.data;
 
-          );
-        const contestantData = response.data;
-
+        setContestantData(fetchedContestantData);
         setFormData({
           description: contestantData.description,
           videoUrl: contestantData.url_video,
         });
       } catch (error) {
-        console.error('Error fetching contestant data:', error);
+        console.error("Error fetching contestant data:", error);
       }
     };
 
@@ -44,34 +52,65 @@ function EditDashboard({ URL, contestantId, toggleEditing,  updateContestantData
     e.preventDefault();
 
     try {
-      const response = await axios.post(`${URL}/contestants/${contestantId}`, {
-        description: formData.description,
-        videoUrl: formData.videoUrl,
-      },
-      { headers: { Authorization: `${API_KEY}` } });
+      const response = await axios.post(
+        `${URL}/contestants/${contestantId}`,
+        {
+          description: formData.description,
+          videoUrl: formData.videoUrl,
+        },
+        { headers: { Authorization: `${API_KEY}` } }
+      );
 
       // When successful, update the state
-      console.log('Contestant data updated successfully:', response.data);
+      console.log("Contestant data updated successfully:", response.data);
       setUpdateSuccess(true);
 
-    setTimeout(() => {
-      toggleEditing();
-      setUpdateSuccess(false); 
-      updateContestantData();
-    }, 3000); 
-
-
+      setTimeout(() => {
+        toggleEditing();
+        setUpdateSuccess(false);
+        updateContestantData();
+      }, 3000);
     } catch (error) {
       // Handle error
-      console.error('Error updating contestant data:', error);
+      console.error("Error updating contestant data:", error);
+      setUpdateError(error.message);
+    }
+  };
+
+  const handleVideoSubmit = async () => {
+    try {
+      const response = await axios.post(
+        `${URL}/contestants/${contestantId}/submit-video`,
+        {
+          videoUrl: formData.videoUrl,
+        },
+        { headers: { Authorization: `${API_KEY}` } }
+      );
+   // Update round to 2
+      await axios.put(
+        `${URL}/contestants/${contestantId}/update-round`,
+        { round: 2 },
+        { headers: { Authorization: `${API_KEY}` } }
+      );
+      console.log("Video submitted successfully:", response.data);
+      setFormData((prevData) => ({
+        ...prevData,
+        videoUrl: "", // Clear the video URL input
+      }));
+
+   
+
+      // Update the contestant data
+      updateContestantData();
+    } catch (error) {
+      console.error("Error submitting video:", error);
       setUpdateError(error.message);
     }
   };
 
   return (
-    <section className='edit'>
-
-        <div className="edit-dashboard">
+    <section className="edit">
+      <div className="edit-dashboard">
         <h2 className="edit-dashboard__title">Edit Dashboard</h2>
         {updateSuccess && (
           <p className="edit-dashboard__success-flash">
@@ -80,41 +119,53 @@ function EditDashboard({ URL, contestantId, toggleEditing,  updateContestantData
         )}
         {updateError && <p className="edit-dashboard__error">{updateError}</p>}
 
-          <form className="edit-dashboard__form" onSubmit={handleFormSubmit}>
-            <label className="edit-dashboard__form__label" htmlFor="description">
-              Description:
-            </label>
-            <p>Edit your description.</p>
-            <textarea
-              className="edit-dashboard__form__textarea"
-              id="description"
-              name="description"
-              value={formData.description}
-              onChange={handleInputChange}
-            />
+        <form className="edit-dashboard__form" onSubmit={handleFormSubmit}>
+          <label className="edit-dashboard__form__label" htmlFor="description">
+            Description:
+          </label>
+          <p>Edit your description.</p>
+          <textarea
+            className="edit-dashboard__form__textarea"
+            id="description"
+            name="description"
+            value={formData.description}
+            onChange={handleInputChange}
+          />
 
-            <label className="edit-dashboard__form__label" htmlFor="videoUrl">
-              Video URL:
-            </label>
-            <p>Videos can only be changed once every voting cycle</p>
-            <input
-              className="edit-dashboard__form__input"
-              type="text"
-              id="videoUrl"
-              name="videoUrl"
-              value={formData.videoUrl}
-              onChange={handleInputChange}
-            />
+          {contestantData.round === 1 && ( // Only show the video URL input and button if round === 1
+            <>
+              <label
+                className="edit-dashboard__form__label"
+                htmlFor="videoUrl"
+              >
+                Video URL:
+              </label>
+              <p>Videos can only be changed once every voting cycle</p>
+              <input
+                className="edit-dashboard__form__input"
+                type="text"
+                id="videoUrl"
+                name="videoUrl"
+                value={formData.videoUrl}
+                onChange={handleInputChange}
+              />
+              <button
+                className="edit-dashboard__form__button"
+                type="button" // Use type="button" for the video submit button
+                onClick={handleVideoSubmit}
+              >
+                Submit Video
+              </button>
+            </>
+          )}
 
-            <button className="edit-dashboard__form__button" type="submit">
-              Update
-            </button>
-          </form>
-        </div>
- 
+          <button className="edit-dashboard__form__button" type="submit">
+            Update
+          </button>
+        </form>
+      </div>
     </section>
   );
 }
 
 export default EditDashboard;
-
