b4be132f3cd92e3c1bf3b906ffb4624667d53b09
Maria Zak <mzakart@gmail.com>
2024-04-12 19:05:06 -0400

    temp secret for testing

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
-import '../../styles/forms.scss';
+import "../../styles/forms.scss";
 import SignUpContestant from "../../components/SignUpContestant/SignUpContestant";
 
-const SignUpPage = ({URL, API_KEY} ) => {
-return(
-  <SignUpContestant URL={URL} API_KEY={API_KEY}/>
-)
-
+const SignUpPage = ({ URL, API_KEY }) => {
+  return <SignUpContestant URL={URL} API_KEY={API_KEY} />;
 };
 
 export default SignUpPage;
